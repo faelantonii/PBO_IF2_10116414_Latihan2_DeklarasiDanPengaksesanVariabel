99c17175a9d1dbc71028c0bcd7d1c03402e820c9
sweenutt <sweenutt@192.168.3.19>
2020-10-20 01:23:03 +0700

    Upload Latihan 1 Biodata

---
+++ src/PBO_IF2_10116414_Latihan2_DeklarasiDanPengaksesanVariabel/PBO_IF2_10116414_DeklarasiDanPengaksesanVariabel.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO_IF2_10116414_Latihan2_DeklarasiDanPengaksesanVariabel;
+
+/**
+ *
+ * @author sweenutt
+ * NAMA         : FAEL ANTONI WIJAYA
+ * KELAS        : PBO-2
+ * NIM          : 10116414
+ * Deskripsi    : Program Deklarasi Dan Pengaksesan Variabel
+ */
+public class PBO_IF2_10116414_DeklarasiDanPengaksesanVariabel {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Deklarasi variabel
+        int nilaiInt;
+        final double PHI = 3.14; //konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //Memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        //Menampilan Hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variable PHi = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("isi variabel karakter = " + nilaiKarakter);              
+    }
+    
+}
